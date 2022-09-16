f6adf01cfe7f5855225f968a0f58f8024096d69d
ivanpachecos <111659681+ivanpachecos@users.noreply.github.com>
2022-09-15 20:54:46 -0500

    Add files via upload

---
+++ FirstMain.java
@@ -0,0 +1,23 @@
+package com.exerce;
+
+public class FirstMain {
+    public static void main(String[] args){
+
+        int edad = 23;
+        long medida = 12;
+        double altura = 13.2;
+
+        boolean verdadero = true;
+        boolean falso = false;
+
+        String nombre = "Ivan";
+
+        System.out.println(edad);
+        System.out.println(medida);
+        System.out.println(altura);
+        System.out.println(verdadero);
+        System.out.println(falso);
+        System.out.println(nombre);
+
+    }
+}
